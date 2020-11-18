7aba4d32fb6ffcbda2dd4da368826d2d9524c8d1
Angel Martz <angel.martz24@gmail.com>
2020-11-18 11:35:26 -0600

    Adding Article Model Fillable

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'author',
+        'body',
+        'photo',
+    ];
 }
